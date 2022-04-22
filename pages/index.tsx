e24072fc379b33a1043bbe379356a9dbd94cd87f
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2022-04-22 15:28:48 -0300

    ➕ Material UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
-import type { NextPage } from 'next'
+import * as React from 'react';
+import type { NextPage } from 'next';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 const Home: NextPage = () => {
   return (
-    <h1>Hello World</h1>
-  )
-}
+    <Container maxWidth='lg'>
+      <Box
+        sx={{
+          my: 5,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component='h1' color='primary'>
+          Material UI v5 with Next.js in TypeScript
+        </Typography>
+        <Typography component='h2' color='secondary'>
+          Boilerplate for building faster.
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
